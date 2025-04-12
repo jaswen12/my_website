3c385f1d7142a50b443b7c984c2cdc39aa32e4bd
Jasper <sho@Jaspers-MacBook-Air-2.local>
2025-04-12 13:52:28 +0800

    small page test

--- public/js/transition.js
+++ public/js/transition.js
@@ -1,20 +1,29 @@
 barba.init({
-  transitions: [{
-    name: 'page-flip',
-    leave(data) {
-      return gsap.to(data.current.container, {
-        rotationY: 90,
-        duration: 0.6,
-        ease: "power2.inOut"
-      });
-    },
-    enter(data) {
-      gsap.set(data.next.container, { rotationY: -90 });
-      return gsap.to(data.next.container, {
-        rotationY: 0,
-        duration: 0.6,
-        ease: "power2.out"
-      });
+  transitions: [
+    {
+      name: 'page-flip',
+      async leave(data) {
+        await gsap.to(data.current.container, {
+          opacity: 0,
+          rotateY: 90,
+          transformOrigin: "left center",
+          duration: 0.6,
+          ease: "power2.inOut"
+        });
+      },
+      enter(data) {
+        gsap.set(data.next.container, {
+          opacity: 0,
+          rotateY: -90,
+          transformOrigin: "right center"
+        });
+        return gsap.to(data.next.container, {
+          opacity: 1,
+          rotateY: 0,
+          duration: 0.6,
+          ease: "power2.out"
+        });
+      }
     }
-  }]
+  ]
 });
