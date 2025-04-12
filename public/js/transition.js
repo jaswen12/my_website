3f6732f224a0b6da94cfb5c4842ae21c471c4dfc
Jasper <sho@Jaspers-MacBook-Air-2.local>
2025-04-12 19:15:13 +0800

    update transition

--- public/js/transition.js
+++ public/js/transition.js
@@ -1,33 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Barba DOM ready");
+  console.log("Barba running");
 
   barba.init({
-    transitions: [
-      {
-        name: 'page-flip',
-        async leave(data) {
-          await gsap.to(data.current.container, {
-            opacity: 0,
-            rotateY: 90,
-            transformOrigin: "left center",
-            duration: 0.6,
-            ease: "power2.inOut"
-          });
-        },
-        enter(data) {
-          gsap.set(data.next.container, {
-            opacity: 0,
-            rotateY: -90,
-            transformOrigin: "right center"
-          });
-          return gsap.to(data.next.container, {
-            opacity: 1,
-            rotateY: 0,
-            duration: 0.6,
-            ease: "power2.out"
-          });
-        }
+    transitions: [{
+      name: 'page-flip',
+      async leave(data) {
+        await gsap.to(data.current.container, {
+          opacity: 0,
+          rotateY: 90,
+          transformOrigin: "left center",
+          duration: 0.6,
+          ease: "power2.inOut"
+        });
+      },
+      enter(data) {
+        gsap.set(data.next.container, {
+          opacity: 0,
+          rotateY: -90,
+          transformOrigin: "right center"
+        });
+        return gsap.to(data.next.container, {
+          opacity: 1,
+          rotateY: 0,
+          duration: 0.6,
+          ease: "power2.out"
+        });
+      },
+      after(data) {
+        //Reset any transforms applied
+        gsap.set(data.current.container, {
+          clearProps: "all"
+        });
+        gsap.set(data.next.container, {
+          clearProps: "all"
+        });
       }
-    ]
+    }]
   });
 });
