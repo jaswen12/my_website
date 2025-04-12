97794dde3bdfde425198fe0d8e2a670d5abe68ef
Jasper <sho@Jaspers-MacBook-Air-2.local>
2025-04-12 14:40:18 +0800

    barba test

--- public/js/transition.js
+++ public/js/transition.js
@@ -1,29 +1,33 @@
-barba.init({
-  transitions: [
-    {
-      name: 'page-flip',
-      async leave(data) {
-        await gsap.to(data.current.container, {
-          opacity: 0,
-          rotateY: 90,
-          transformOrigin: "left center",
-          duration: 0.6,
-          ease: "power2.inOut"
-        });
-      },
-      enter(data) {
-        gsap.set(data.next.container, {
-          opacity: 0,
-          rotateY: -90,
-          transformOrigin: "right center"
-        });
-        return gsap.to(data.next.container, {
-          opacity: 1,
-          rotateY: 0,
-          duration: 0.6,
-          ease: "power2.out"
-        });
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Barba is active and transitions initialized");
+
+  barba.init({
+    transitions: [
+      {
+        name: 'page-flip',
+        async leave(data) {
+          await gsap.to(data.current.container, {
+            opacity: 0,
+            rotateY: 90,
+            transformOrigin: "left center",
+            duration: 0.6,
+            ease: "power2.inOut"
+          });
+        },
+        enter(data) {
+          gsap.set(data.next.container, {
+            opacity: 0,
+            rotateY: -90,
+            transformOrigin: "right center"
+          });
+          return gsap.to(data.next.container, {
+            opacity: 1,
+            rotateY: 0,
+            duration: 0.6,
+            ease: "power2.out"
+          });
+        }
       }
-    }
-  ]
+    ]
+  });
 });
